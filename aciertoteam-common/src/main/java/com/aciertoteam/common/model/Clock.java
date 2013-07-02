e3abe5792ccec63ec3cfaddd5a5447ab50c86725
Bogdan Nechyporenko <bogdannechiporenko@gmail.com>
2013-07-02 12:53:59 -0700

    test added

---
+++ aciertoteam-common/src/main/java/com/aciertoteam/common/model/Clock.java
@@ -0,0 +1,15 @@
+package com.aciertoteam.common.model;
+
+import java.util.Date;
+
+/**
+ * Wrapper class over time.
+ *
+ * @author Bogdan Nechyporenko
+ */
+public class Clock {
+
+    public Date getCurrentDate() {
+        return new Date();
+    }
+}
--- aciertoteam-common/src/main/java/com/aciertoteam/common/repository/impl/DefaultEntityRepository.java
+++ aciertoteam-common/src/main/java/com/aciertoteam/common/repository/impl/DefaultEntityRepository.java
@@ -2,13 +2,14 @@ package com.aciertoteam.common.repository.impl;
 
 import com.aciertoteam.common.entity.AbstractEntity;
 import com.aciertoteam.common.interfaces.IAbstractEntity;
+import com.aciertoteam.common.model.Clock;
 import com.aciertoteam.common.repository.EntityRepository;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,23 +20,26 @@ import java.util.List;
 @Transactional
 public class DefaultEntityRepository extends DefaultAbstractRepository<AbstractEntity> implements EntityRepository {
 
+    @Autowired
+    private Clock clock;
+
     public long count(Class clazz) {
         return Long.valueOf(String.valueOf(getSession().createCriteria(clazz).
-                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", new Date()))).
+                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", clock.getCurrentDate()))).
                 setProjection(Projections.rowCount()).uniqueResult()));
     }
 
     @Override
     public <T> T findByField(Class clazz, String fieldName, Object value) {
         return (T) getSession().createCriteria(clazz).add(Restrictions.eq(fieldName, value)).
-                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", new Date()))).
+                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", clock.getCurrentDate()))).
                 uniqueResult();
     }
 
     @Override
     public <T> T findById(Class clazz, Long id) {
         return (T) getSession().createCriteria(clazz).add(Restrictions.eq("id", id)).
-                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", new Date()))).
+                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", clock.getCurrentDate()))).
                 uniqueResult();
     }
 
@@ -46,20 +50,20 @@ public class DefaultEntityRepository extends DefaultAbstractRepository<AbstractE
     @Override
     public <T> List<T> findByIds(Class<T> clazz, List<Long> ids) {
         return getSession().createCriteria(clazz).add(Restrictions.in("id", ids)).
-                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", new Date()))).
+                add(Restrictions.or(Restrictions.isNull("validThru"), Restrictions.gt("validThru", clock.getCurrentDate()))).
                 list();
     }
 
     @Override
     public <T extends IAbstractEntity> List<T> findAll(Class<T> clazz) {
         return getSession().createQuery("from " + clazz.getSimpleName() + " where validThru is null or validThru > :now").
-                setParameter("now", new Date()).list();
+                setParameter("now", clock.getCurrentDate()).list();
     }
 
     @Override
     public <T extends IAbstractEntity> List<T> findAllOrderedByTimestamp(Class<T> clazz, boolean asc) {
         return getSession().createQuery("from " + clazz.getSimpleName() + " where validThru is null or validThru > :now" +
                 " order by timestamp " + (asc ? "asc" : "desc")).
-                setParameter("now", new Date()).list();
+                setParameter("now", clock.getCurrentDate()).list();
     }
 }
