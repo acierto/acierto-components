a39ac01dcacc50339e22246e7e68a02439d11195
Bogdan Nechyporenko <bogdannechiporenko@gmail.com>
2013-07-07 06:37:50 -0700

    Test cases are added

---
+++ aciertoteam-common/src/main/java/com/aciertoteam/common/utils/SiteContentReader.java
@@ -0,0 +1,51 @@
+package com.aciertoteam.common.utils;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.CharEncoding;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.ContentEncodingHttpClient;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * @author Bogdan Nechyporenko
+ */
+public class SiteContentReader {
+
+    private static final Logger LOG = Logger.getLogger(SiteContentReader.class);
+
+    @SuppressWarnings("unchecked")
+    public static InputStream readAsInputStream(String url) {
+
+        HttpClient httpclient = new ContentEncodingHttpClient();
+        HttpGet httpget = new HttpGet(url);
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+            return response.getEntity().getContent();
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static String readContent(String url) {
+
+        HttpClient httpclient = new ContentEncodingHttpClient();
+        HttpGet httpget = new HttpGet(url);
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+            return IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8).trim();
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+            return null;
+        }
+    }
+
+
+}
