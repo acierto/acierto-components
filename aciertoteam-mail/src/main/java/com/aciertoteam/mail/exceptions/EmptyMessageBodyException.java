eb17a6a181207334f2a39fe571a18659ba917a17
Bogdan Nechyporenko <bogdannechiporenko@gmail.com>
2013-07-23 14:34:07 -0700

    removed redundant code

--- aciertoteam-mail/src/main/java/com/aciertoteam/mail/exceptions/EmptyMessageBodyException.java
+++ aciertoteam-mail/src/main/java/com/aciertoteam/mail/exceptions/EmptyMessageBodyException.java
@@ -8,16 +8,4 @@ public class EmptyMessageBodyException extends RuntimeException {
     public EmptyMessageBodyException() {
         super();
     }
-
-    public EmptyMessageBodyException(String message) {
-        super(message);
-    }
-
-    public EmptyMessageBodyException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public EmptyMessageBodyException(Throwable cause) {
-        super(cause);
-    }
 }
