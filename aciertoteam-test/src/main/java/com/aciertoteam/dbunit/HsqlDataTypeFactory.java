0db5aa7785172fac075c120151012edf6891d2a5
Zubastick <IgorShestiporov@gmail.com>
2013-08-26 05:18:50 -0700

    Fixed tests

--- aciertoteam-test/src/main/java/com/aciertoteam/dbunit/HsqlDataTypeFactory.java
+++ aciertoteam-test/src/main/java/com/aciertoteam/dbunit/HsqlDataTypeFactory.java
@@ -1,21 +1,30 @@
 package com.aciertoteam.dbunit;
 
 import java.sql.Types;
+import java.util.Arrays;
+import java.util.Collection;
 import org.dbunit.dataset.datatype.DataType;
 import org.dbunit.dataset.datatype.DataTypeException;
 import org.dbunit.dataset.datatype.DefaultDataTypeFactory;
 
 /**
  * Creates a factory
- *
+ * 
  * @author Bogdan Nechyporenko
  */
 public class HsqlDataTypeFactory extends DefaultDataTypeFactory {
+
+    private static final Collection DATABASE_PRODUCTS = Arrays.asList("derby", "H2");
 
     public DataType createDataType(int sqlType, String sqlTypeName) throws DataTypeException {
         if (sqlType == Types.BOOLEAN) {
             return DataType.BOOLEAN;
         }
         return super.createDataType(sqlType, sqlTypeName);
+    }
+
+    @Override
+    public Collection getValidDbProducts() {
+        return DATABASE_PRODUCTS;
     }
 }
