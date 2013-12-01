44ada2f3e63036dd18ee5e6abebc80521c74f3cb
Bogdan Nechyporenko <bogdannechiporenko@gmail.com>
2013-12-01 14:32:02 -0800

    Tests fixed

--- aciertoteam-geo/src/main/java/com/aciertoteam/geo/entity/Language.java
+++ aciertoteam-geo/src/main/java/com/aciertoteam/geo/entity/Language.java
@@ -23,7 +23,7 @@ public class Language extends AbstractEntity {
     @Column(unique = true, nullable = false)
     private String englishName;
 
-    @Column(unique = true, nullable = false)
+    @Column//(unique = true, nullable = false)
     private String nativeLanguageName;
 
     Language() {
